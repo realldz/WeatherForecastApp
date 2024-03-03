f36fd3041811deaf95408125fc52403ea6892b6a
fm39hz-desktop <hitpoint2k3@gmail.com>
2024-03-04 02:11:46 +0700

    feat: add style config for HomePage

---
+++ src/screens/Home/style.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+	mainBorder: {
+		flex: 1,
+		backgroundColor: 'red',
+	},
+	textPreview: {
+		textAlign: 'center',
+	},
+});
+
+export default styles;
