e8595cf08d1e6731d2f4576c3ff74b2fd79f4895
fm39hz-workstation <hitpoint2k3@gmail.com>
2024-04-14 22:57:45 +0700

    feat: create mockData

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -3,21 +3,40 @@ import { ScrollView, View } from 'react-native';
 import styles from '@/screens/Home/style';
 import { DailyWeather } from '@/components/molecules';
 
+const mockData = [
+	{
+		date: 'Wed 16',
+		icon: <></>,
+		temperature: '22 ',
+		wind: '1-5 km/h',
+	},
+	{
+		date: 'Thu 17',
+		icon: <></>,
+		temperature: '25 ',
+		wind: '0-3 km/h',
+	},
+	{
+		date: 'Fri 18',
+		icon: <></>,
+		temperature: '19 ',
+		wind: '4-8 km/h',
+	},
+];
 const Home = () => {
-	return (
-		<SafeScreen>
-			<ScrollView style={styles.mainBorder}>
-				<View>
-					<DailyWeather
-						date="Wed 16"
-						icon={<></>}
-						temperature="22 "
-						wind="1-5 km/h"
-					/>
-				</View>
-			</ScrollView>
-		</SafeScreen>
-	);
+	return <SafeScreen>
+		<ScrollView style={styles.mainBorder}>
+			<View style={styles.dailyView}>
+				{mockData.map((item, index) => <DailyWeather
+					key={index}
+					date={item.date}
+					icon={item.icon}
+					temperature={item.temperature}
+					wind={item.wind}
+				/>)}
+			</View>
+		</ScrollView>
+	</SafeScreen>;
 };
 
 export default Home;
