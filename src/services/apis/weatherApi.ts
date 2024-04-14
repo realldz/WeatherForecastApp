1ab85ad507877b125dec35da67068d3fc550d1b6
fm39hz-workstation <hitpoint2k3@gmail.com>
2024-04-14 22:56:37 +0700

    feat: move api url to env file

--- src/services/apis/weatherApi.ts
+++ src/services/apis/weatherApi.ts
@@ -1,9 +1,9 @@
 import { fetchWeatherApi } from 'openmeteo';
 import { WeatherParamsExtend } from '@/interfaces/ForecastParams';
 
-const URL = 'https://api.open-meteo.com/v1/forecast';
+const API_URL = process.env.API_URL;
 const getTemperature = async (params: WeatherParamsExtend) => {
-	return fetchWeatherApi(URL, params);
+	return fetchWeatherApi(API_URL!, params);
 };
 
 export { getTemperature };
