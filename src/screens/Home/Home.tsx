6b46ef0262bf7ffca11d3614761a3244e6f46b5e
fm39hz-desktop <hitpoint2k3@gmail.com>
2024-04-24 01:06:57 +0700

    feat: add weather icons

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,24 +1,25 @@
 import { SafeScreen } from '@/components/template';
 import { ScrollView, View } from 'react-native';
 import styles from '@/screens/Home/style';
-import { DailyWeather } from '@/components/molecules';
+import { WeatherStatus } from '@/components/molecules';
+import { WeatherIcon } from 'weather-react-icons';
 
 const mockData = [
 	{
 		date: 'Wed 16',
-		icon: <></>,
+		icon: <WeatherIcon iconId={200} name="owm" />,
 		temperature: '22 ',
 		wind: '1-5 km/h',
 	},
 	{
 		date: 'Thu 17',
-		icon: <></>,
+		icon: <WeatherIcon iconId={200} name="owm" />,
 		temperature: '25 ',
 		wind: '0-3 km/h',
 	},
 	{
 		date: 'Fri 18',
-		icon: <></>,
+		icon: <WeatherIcon iconId={200} name="owm" />,
 		temperature: '19 ',
 		wind: '4-8 km/h',
 	},
@@ -29,7 +30,7 @@ const Home = () => {
 			<ScrollView style={styles.mainBorder}>
 				<View style={styles.dailyView}>
 					{mockData.map((item, index) => (
-						<DailyWeather
+						<WeatherStatus
 							key={index}
 							date={item.date}
 							icon={item.icon}
